c72f68ee7fbd49f49e1316543fae7838e59b786b
wangpwm <wangpwm@yonyou.com>
2022-11-06 13:51:18 +0800

    ray

--- install-v2ray.sh
+++ install-v2ray.sh
@@ -27,15 +27,15 @@ OK="${Green}[OK]${Font}"
 ERROR="${Red}[ERROR]${Font}"
 
 # 变量
+shell_version="1.3.11"
 github_repo="https://raw.githubusercontent.com/aixohub/v2ray_onekey"
 github_branch="main"
-shell_version="1.3.11"
-xray_conf_dir="/usr/local/etc/xray"
-website_dir="/www/xray_web/"
-xray_access_log="/var/log/xray/access.log"
-xray_error_log="/var/log/xray/error.log"
-cert_dir="/usr/local/etc/xray"
-domain_tmp_dir="/usr/local/etc/xray"
+v2ray_conf_dir="/usr/local/etc/v2ray"
+website_dir="/www/v2ray_web/"
+v2ray_access_log="/var/log/v2ray/access.log"
+v2ray_error_log="/var/log/v2ray/error.log"
+cert_dir="/usr/local/etc/v2ray"
+domain_tmp_dir="/usr/local/etc/v2ray"
 cert_group="nobody"
 random_num=$((RANDOM % 12 + 4))
 
@@ -43,8 +43,8 @@ VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
 WS_PATH="/$(head -n 10 /dev/urandom | md5sum | head -c ${random_num})/"
 
 function shell_mode_check() {
-  if [ -f ${xray_conf_dir}/config.json ]; then
-    if [ "$(grep -c "wsSettings" ${xray_conf_dir}/config.json)" -ge 1 ]; then
+  if [ -f ${v2ray_conf_dir}/config.json ]; then
+    if [ "$(grep -c "wsSettings" ${v2ray_conf_dir}/config.json)" -ge 1 ]; then
       shell_mode="ws"
     else
       shell_mode="tcp"
@@ -214,7 +214,7 @@ function dependency_install() {
     judge "安装 jq"
   fi
 
-  # 防止部分系统xray的默认bin目录缺失
+  # 防止部分系统v2ray的默认bin目录缺失
   mkdir /usr/local/bin >/dev/null 2>&1
 }
 
@@ -261,7 +261,7 @@ function domain_check() {
     print_ok "域名通过 DNS 解析的 IP 地址与 本机 IPv6 地址匹配"
     sleep 2
   else
-    print_error "请确保域名添加了正确的 A / AAAA 记录，否则将无法正常使用 xray"
+    print_error "请确保域名添加了正确的 A / AAAA 记录，否则将无法正常使用 v2ray"
     print_error "域名通过 DNS 解析的 IP 地址与 本机 IPv4 / IPv6 地址不匹配，是否继续安装？（y/n）" && read -r install
     case $install in
     [yY][eE][sS] | [yY])
@@ -310,36 +310,36 @@ function update_sh() {
   fi
 }
 
-function xray_tmp_config_file_check_and_use() {
-  if [[ -s ${xray_conf_dir}/config_tmp.json ]]; then
-    mv -f ${xray_conf_dir}/config_tmp.json ${xray_conf_dir}/config.json
+function v2ray_tmp_config_file_check_and_use() {
+  if [[ -s ${v2ray_conf_dir}/config_tmp.json ]]; then
+    mv -f ${v2ray_conf_dir}/config_tmp.json ${v2ray_conf_dir}/config.json
   else
-    print_error "xray 配置文件修改异常"
+    print_error "v2ray 配置文件修改异常"
   fi
 }
 
 function modify_UUID() {
   [ -z "$UUID" ] && UUID=$(cat /proc/sys/kernel/random/uuid)
-  cat ${xray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"settings","clients",0,"id"];"'${UUID}'")' >${xray_conf_dir}/config_tmp.json
-  xray_tmp_config_file_check_and_use
+  cat ${v2ray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"settings","clients",0,"id"];"'${UUID}'")' >${v2ray_conf_dir}/config_tmp.json
+  v2ray_tmp_config_file_check_and_use
   judge "Xray TCP UUID 修改"
 }
 
 function modify_UUID_ws() {
-  cat ${xray_conf_dir}/config.json | jq 'setpath(["inbounds",1,"settings","clients",0,"id"];"'${UUID}'")' >${xray_conf_dir}/config_tmp.json
-  xray_tmp_config_file_check_and_use
+  cat ${v2ray_conf_dir}/config.json | jq 'setpath(["inbounds",1,"settings","clients",0,"id"];"'${UUID}'")' >${v2ray_conf_dir}/config_tmp.json
+  v2ray_tmp_config_file_check_and_use
   judge "Xray ws UUID 修改"
 }
 
 function modify_fallback_ws() {
-  cat ${xray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"settings","fallbacks",2,"path"];"'${WS_PATH}'")' >${xray_conf_dir}/config_tmp.json
-  xray_tmp_config_file_check_and_use
+  cat ${v2ray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"settings","fallbacks",2,"path"];"'${WS_PATH}'")' >${v2ray_conf_dir}/config_tmp.json
+  v2ray_tmp_config_file_check_and_use
   judge "Xray fallback_ws 修改"
 }
 
 function modify_ws() {
-  cat ${xray_conf_dir}/config.json | jq 'setpath(["inbounds",1,"streamSettings","wsSettings","path"];"'${WS_PATH}'")' >${xray_conf_dir}/config_tmp.json
-  xray_tmp_config_file_check_and_use
+  cat ${v2ray_conf_dir}/config.json | jq 'setpath(["inbounds",1,"streamSettings","wsSettings","path"];"'${WS_PATH}'")' >${v2ray_conf_dir}/config_tmp.json
+  v2ray_tmp_config_file_check_and_use
   judge "Xray ws 修改"
 }
 
@@ -361,19 +361,19 @@ function modify_port() {
     exit 1
   fi
   port_exist_check $PORT
-  cat ${xray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"port"];'${PORT}')' >${xray_conf_dir}/config_tmp.json
-  xray_tmp_config_file_check_and_use
+  cat ${v2ray_conf_dir}/config.json | jq 'setpath(["inbounds",0,"port"];'${PORT}')' >${v2ray_conf_dir}/config_tmp.json
+  v2ray_tmp_config_file_check_and_use
   judge "Xray 端口 修改"
 }
 
-function configure_xray() {
-  cd /usr/local/etc/xray && rm -f config.json && wget -O config.json ${github_repo}/${github_branch}/config/xray_xtls-rprx-direct.json
+function configure_v2ray() {
+  cd /usr/local/etc/v2ray && rm -f config.json && wget -O config.json ${github_repo}/${github_branch}/config/xtls-rprx-direct.json
   modify_UUID
   modify_port
 }
 
-function configure_xray_ws() {
-  cd /usr/local/etc/xray && rm -f config.json && wget -O config.json ${github_repo}/${github_branch}/config/xray_tls_ws_mix-rprx-direct.json
+function configure_v2ray_ws() {
+  cd /usr/local/etc/v2ray && rm -f config.json && wget -O config.json ${github_repo}/${github_branch}/config/tls_ws_mix-rprx-direct.json
   modify_UUID
   modify_UUID_ws
   modify_port
@@ -381,10 +381,10 @@ function configure_xray_ws() {
   modify_ws
 }
 
-function xray_install() {
-  print_ok "安装 Xray"
-  curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh | bash -s -- install
-  judge "Xray 安装"
+function v2ray_install() {
+  print_ok "安装 v2ray"
+  bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+  judge "v2ray 安装"
 
   # 用于生成 Xray 的导入链接
   echo $domain >$domain_tmp_dir/domain
@@ -414,7 +414,7 @@ function acme() {
   if "$HOME"/.acme.sh/acme.sh --issue --insecure -d "${domain}" --webroot "$website_dir" -k ec-256 --force; then
     print_ok "SSL 证书生成成功"
     sleep 2
-    if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/xray.crt --keypath /ssl/xray.key --reloadcmd "systemctl restart xray" --ecc --force; then
+    if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/v2ray.crt --keypath /ssl/v2ray.key --reloadcmd "systemctl restart v2ray" --ecc --force; then
       print_ok "SSL 证书配置成功"
       sleep 2
       if [[ -n $(type -P wgcf) && -n $(type -P wg-quick) ]]; then
@@ -425,7 +425,7 @@ function acme() {
   elif "$HOME"/.acme.sh/acme.sh --issue --insecure -d "${domain}" --webroot "$website_dir" -k ec-256 --force --listen-v6; then
     print_ok "SSL 证书生成成功"
     sleep 2
-    if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/xray.crt --keypath /ssl/xray.key --reloadcmd "systemctl restart xray" --ecc --force; then
+    if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/v2ray.crt --keypath /ssl/v2ray.key --reloadcmd "systemctl restart v2ray" --ecc --force; then
       print_ok "SSL 证书配置成功"
       sleep 2
       if [[ -n $(type -P wgcf) && -n $(type -P wg-quick) ]]; then
@@ -450,7 +450,7 @@ function acme() {
 function ssl_judge_and_install() {
 
   mkdir -p /ssl >/dev/null 2>&1
-  if [[ -f "/ssl/xray.key" || -f "/ssl/xray.crt" ]]; then
+  if [[ -f "/ssl/v2ray.key" || -f "/ssl/v2ray.crt" ]]; then
     print_ok "/ssl 目录下证书文件已存在"
     print_ok "是否删除 /ssl 目录下的证书文件 [Y/N]?"
     read -r ssl_delete
@@ -464,16 +464,16 @@ function ssl_judge_and_install() {
     esac
   fi
 
-  if [[ -f "/ssl/xray.key" || -f "/ssl/xray.crt" ]]; then
+  if [[ -f "/ssl/v2ray.key" || -f "/ssl/v2ray.crt" ]]; then
     echo "证书文件已存在"
   elif [[ -f "$HOME/.acme.sh/${domain}_ecc/${domain}.key" && -f "$HOME/.acme.sh/${domain}_ecc/${domain}.cer" ]]; then
     echo "证书文件已存在"
-    "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/xray.crt --keypath /ssl/xray.key --ecc
+    "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/v2ray.crt --keypath /ssl/v2ray.key --ecc
     judge "证书启用"
   else
     mkdir /ssl
-    cp -a $cert_dir/self_signed_cert.pem /ssl/xray.crt
-    cp -a $cert_dir/self_signed_key.pem /ssl/xray.key
+    cp -a $cert_dir/self_signed_cert.pem /ssl/v2ray.crt
+    cp -a $cert_dir/self_signed_key.pem /ssl/v2ray.key
     ssl_install
     acme
   fi
@@ -484,9 +484,9 @@ function ssl_judge_and_install() {
 
 function generate_certificate() {
   if [[ -z ${local_ipv4} && -n ${local_ipv6} ]]; then
-    signedcert=$(xray tls cert -domain="$local_ipv6" -name="$local_ipv6" -org="$local_ipv6" -expire=87600h)
+    signedcert=$(v2ray tls cert -domain="$local_ipv6" -name="$local_ipv6" -org="$local_ipv6" -expire=87600h)
   else
-    signedcert=$(xray tls cert -domain="$local_ipv4" -name="$local_ipv4" -org="$local_ipv4" -expire=87600h)
+    signedcert=$(v2ray tls cert -domain="$local_ipv4" -name="$local_ipv4" -org="$local_ipv4" -expire=87600h)
   fi
   echo $signedcert | jq '.certificate[]' | sed 's/\"//g' | tee $cert_dir/self_signed_cert.pem
   echo $signedcert | jq '.key[]' | sed 's/\"//g' >$cert_dir/self_signed_key.pem
@@ -497,14 +497,14 @@ function generate_certificate() {
 }
 
 function configure_web() {
-  rm -rf /www/xray_web
-  mkdir -p /www/xray_web
+  rm -rf /www/v2ray_web
+  mkdir -p /www/v2ray_web
   print_ok "是否配置伪装网页？[Y/N]"
   read -r webpage
   case $webpage in
   [yY][eE][sS] | [yY])
     wget -O web.tar.gz ${github_repo}/main/basic/web.tar.gz
-    tar xzf web.tar.gz -C /www/xray_web
+    tar xzf web.tar.gz -C /www/v2ray_web
     judge "站点伪装"
     rm -f web.tar.gz
     ;;
@@ -512,7 +512,7 @@ function configure_web() {
   esac
 }
 
-function xray_uninstall() {
+function v2ray_uninstall() {
   curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh | bash -s -- remove --purge
   rm -rf $website_dir
   print_ok "是否卸载nginx [Y/N]?"
@@ -544,14 +544,14 @@ function xray_uninstall() {
 function restart_all() {
   systemctl restart nginx
   judge "Nginx 启动"
-  systemctl restart xray
+  systemctl restart v2ray
   judge "Xray 启动"
 }
 
 function vless_xtls-rprx-direct_link() {
-  UUID=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
-  PORT=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].port)
-  FLOW=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
+  UUID=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
+  PORT=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].port)
+  FLOW=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
   DOMAIN=$(cat ${domain_tmp_dir}/domain)
 
   print_ok "URL 链接 (VLESS + TCP + TLS)"
@@ -568,9 +568,9 @@ function vless_xtls-rprx-direct_link() {
 }
 
 function vless_xtls-rprx-direct_information() {
-  UUID=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
-  PORT=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].port)
-  FLOW=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
+  UUID=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
+  PORT=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].port)
+  FLOW=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
   DOMAIN=$(cat ${domain_tmp_dir}/domain)
 
   echo -e "${Red} Xray 配置信息 ${Font}"
@@ -585,10 +585,10 @@ function vless_xtls-rprx-direct_information() {
 }
 
 function ws_information() {
-  UUID=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
-  PORT=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].port)
-  FLOW=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
-  WS_PATH=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.fallbacks[2].path | tr -d '"')
+  UUID=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
+  PORT=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].port)
+  FLOW=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
+  WS_PATH=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.fallbacks[2].path | tr -d '"')
   DOMAIN=$(cat ${domain_tmp_dir}/domain)
 
   echo -e "${Red} Xray 配置信息 ${Font}"
@@ -603,10 +603,10 @@ function ws_information() {
 }
 
 function ws_link() {
-  UUID=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
-  PORT=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].port)
-  FLOW=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
-  WS_PATH=$(cat ${xray_conf_dir}/config.json | jq .inbounds[0].settings.fallbacks[2].path | tr -d '"')
+  UUID=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].id | tr -d '"')
+  PORT=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].port)
+  FLOW=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.clients[0].flow | tr -d '"')
+  WS_PATH=$(cat ${v2ray_conf_dir}/config.json | jq .inbounds[0].settings.fallbacks[2].path | tr -d '"')
   WS_PATH_WITHOUT_SLASH=$(echo $WS_PATH | tr -d '/')
   DOMAIN=$(cat ${domain_tmp_dir}/domain)
 
@@ -644,11 +644,11 @@ function basic_ws_information() {
 }
 
 function show_access_log() {
-  [ -f ${xray_access_log} ] && tail -f ${xray_access_log} || echo -e "${RedBG}log 文件不存在${Font}"
+  [ -f ${v2ray_access_log} ] && tail -f ${v2ray_access_log} || echo -e "${RedBG}log 文件不存在${Font}"
 }
 
 function show_error_log() {
-  [ -f ${xray_error_log} ] && tail -f ${xray_error_log} || echo -e "${RedBG}log 文件不存在${Font}"
+  [ -f ${v2ray_error_log} ] && tail -f ${v2ray_error_log} || echo -e "${RedBG}log 文件不存在${Font}"
 }
 
 function bbr_boost_sh() {
@@ -660,15 +660,15 @@ function mtproxy_sh() {
   wget -N --no-check-certificate "https://github.com/wulabing/mtp/raw/master/mtproxy.sh" && chmod +x mtproxy.sh && bash mtproxy.sh
 }
 
-function install_xray() {
+function install_v2ray() {
   is_root
   system_check
   dependency_install
   basic_optimization
   domain_check
   port_exist_check 80
-  xray_install
-  configure_xray
+  v2ray_install
+  configure_v2ray
   nginx_install
   configure_nginx
   configure_web
@@ -677,15 +677,15 @@ function install_xray() {
   restart_all
   basic_information
 }
-function install_xray_ws() {
+function install_v2ray_ws() {
   is_root
   system_check
   dependency_install
   basic_optimization
   domain_check
   port_exist_check 80
-  xray_install
-  configure_xray_ws
+  v2ray_install
+  configure_v2ray_ws
   nginx_install
   configure_nginx
   configure_web
@@ -729,10 +729,10 @@ menu() {
     update_sh
     ;;
   1)
-    install_xray
+    install_v2ray
     ;;
   2)
-    install_xray_ws
+    install_v2ray_ws
     ;;
   11)
     read -rp "请输入 UUID:" UUID
@@ -750,7 +750,7 @@ menu() {
     ;;
   14)
     if [[ ${shell_mode} == "ws" ]]; then
-      read -rp "请输入路径(示例：/wulabing/ 要求两侧都包含 /):" WS_PATH
+      read -rp "请输入路径(示例：/aixohub/ 要求两侧都包含 /):" WS_PATH
       modify_fallback_ws
       modify_ws
       restart_all
@@ -759,20 +759,20 @@ menu() {
     fi
     ;;
   21)
-    tail -f $xray_access_log
+    tail -f $v2ray_access_log
     ;;
   22)
-    tail -f $xray_error_log
+    tail -f $v2ray_error_log
     ;;
   23)
-    if [[ -f $xray_conf_dir/config.json ]]; then
+    if [[ -f $v2ray_conf_dir/config.json ]]; then
       if [[ ${shell_mode} == "tcp" ]]; then
         basic_information
       elif [[ ${shell_mode} == "ws" ]]; then
         basic_ws_information
       fi
     else
-      print_error "xray 配置文件不存在"
+      print_error "v2ray 配置文件不存在"
     fi
     ;;
   31)
@@ -783,14 +783,14 @@ menu() {
     ;;
   33)
     source '/etc/os-release'
-    xray_uninstall
+    v2ray_uninstall
     ;;
   34)
-    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" - install
+    bash -c "$(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)" - install
     restart_all
     ;;
   35)
-    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" - install --beta
+    bash -c "$(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)" - install --beta
     restart_all
     ;;
   36)
