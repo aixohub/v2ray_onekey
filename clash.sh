2419b8b3a10a0b5646d3e1382f68af2df340fe95
wangpengwei <wangpengwei@baidu.com>
2022-11-26 12:52:22 +0800

    go install

---
+++ clash.sh
@@ -0,0 +1,383 @@
+#!/usr/bin/env bash
+
+#====================================================
+#	System Request:Debian 9+/Ubuntu 18.04+/Centos 7+
+#	Author:	aixohub
+#	Dscription: Xray onekey Management
+#	email: admin@aixohub.com
+#====================================================
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+stty erase ^?
+
+cd "$(
+  cd "$(dirname "$0")" || exit
+  pwd
+)" || exit
+
+# 字体颜色配置
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Font="\033[0m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+OK="${Green}[OK]${Font}"
+ERROR="${Red}[ERROR]${Font}"
+
+# 变量
+[ -z "$GOROOT" ] && GOROOT="$HOME/.go"
+[ -z "$GOPATH" ] && GOPATH="$HOME/go"
+
+OS="$(uname -s)"
+ARCH="$(uname -m)"
+
+github_repo="https://raw.githubusercontent.com/aixohub/v2ray_onekey"
+github_branch="main"
+shell_version="1.3.11"
+xray_conf_dir="/usr/local/etc/xray"
+website_dir="/www/xray_web/"
+xray_access_log="/var/log/xray/access.log"
+xray_error_log="/var/log/xray/error.log"
+cert_dir="/usr/local/etc/xray"
+domain_tmp_dir="/usr/local/etc/xray"
+cert_group="nobody"
+random_num=$((RANDOM % 12 + 4))
+
+VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
+WS_PATH="/$(head -n 10 /dev/urandom | md5sum | head -c ${random_num})/"
+
+function shell_mode_check() {
+  if [ -f ${xray_conf_dir}/config.json ]; then
+    if [ "$(grep -c "wsSettings" ${xray_conf_dir}/config.json)" -ge 1 ]; then
+      shell_mode="ws"
+    else
+      shell_mode="tcp"
+    fi
+  else
+    shell_mode="None"
+  fi
+}
+function print_ok() {
+  echo -e "${OK} ${Blue} $1 ${Font}"
+}
+
+function print_error() {
+  echo -e "${ERROR} ${RedBG} $1 ${Font}"
+}
+
+function is_root() {
+  if [[ 0 == "$UID" ]]; then
+    print_ok "当前用户是 root 用户，开始安装流程"
+  else
+    print_error "当前用户不是 root 用户，请切换到 root 用户后重新执行脚本"
+    exit 1
+  fi
+}
+
+judge() {
+  if [[ 0 -eq $? ]]; then
+    print_ok "$1 完成"
+    sleep 1
+  else
+    print_error "$1 失败"
+    exit 1
+  fi
+}
+
+function system_check() {
+  source '/etc/os-release'
+
+  if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
+    print_ok "当前系统为 Centos ${VERSION_ID} ${VERSION}"
+    INS="yum install -y"
+    ${INS} wget
+    wget -N -P /etc/yum.repos.d/ ${github_repo}/${github_branch}/basic/nginx.repo
+
+
+  elif [[ "${ID}" == "ol" ]]; then
+    print_ok "当前系统为 Oracle Linux ${VERSION_ID} ${VERSION}"
+    INS="yum install -y"
+    wget -N -P /etc/yum.repos.d/ ${github_repo}/${github_branch}/basic/nginx.repo
+  elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 9 ]]; then
+    print_ok "当前系统为 Debian ${VERSION_ID} ${VERSION}"
+    INS="apt install -y"
+    # 清除可能的遗留问题
+    rm -f /etc/apt/sources.list.d/nginx.list
+    # nginx 安装预处理
+    $INS curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+    http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list
+    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | tee /etc/apt/preferences.d/99nginx
+
+    apt update
+
+  elif [[ "${ID}" == "ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 18 ]]; then
+    print_ok "当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME}"
+    INS="apt install -y"
+    # 清除可能的遗留问题
+    rm -f /etc/apt/sources.list.d/nginx.list
+    # nginx 安装预处理
+    $INS curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+    http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list
+    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | tee /etc/apt/preferences.d/99nginx
+
+    apt update
+  else
+    print_error "当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内"
+    exit 1
+  fi
+
+  if [[ $(grep "nogroup" /etc/group) ]]; then
+    cert_group="nogroup"
+  fi
+
+  $INS dbus
+
+  # 关闭各类防火墙
+  systemctl stop firewalld
+  systemctl disable firewalld
+  systemctl stop nftables
+  systemctl disable nftables
+  systemctl stop ufw
+  systemctl disable ufw
+}
+
+function nginx_install() {
+  if ! command -v nginx >/dev/null 2>&1; then
+    ${INS} nginx
+    judge "Nginx 安装"
+  else
+    print_ok "Nginx 已存在"
+  fi
+  # 遗留问题处理
+  mkdir -p /etc/nginx/conf.d >/dev/null 2>&1
+}
+
+
+function dependency_install() {
+  ${INS} lsof tar
+  judge "安装 lsof tar"
+
+
+  ${INS} unzip
+  judge "安装 unzip"
+
+  ${INS} curl
+  judge "安装 curl"
+
+  # upgrade systemd
+  ${INS} systemd
+  judge "安装/升级 systemd"
+
+  ${INS} go
+  judge "安装/升级 golang"
+
+  if [[ "${ID}" == "centos" ]]; then
+    ${INS} pcre pcre-devel zlib-devel epel-release openssl openssl-devel
+  elif [[ "${ID}" == "ol" ]]; then
+    ${INS} pcre pcre-devel zlib-devel openssl openssl-devel
+    # Oracle Linux 不同日期版本的 VERSION_ID 比较乱 直接暴力处理。如出现问题或有更好的方案，请提交 Issue。
+    yum-config-manager --enable ol7_developer_EPEL >/dev/null 2>&1
+    yum-config-manager --enable ol8_developer_EPEL >/dev/null 2>&1
+  else
+    ${INS} libpcre3 libpcre3-dev zlib1g-dev openssl libssl-dev
+  fi
+
+  ${INS} jq
+
+  if ! command -v jq; then
+    wget -P /usr/bin ${github_repo}/${github_branch}/binary/jq && chmod +x /usr/bin/jq
+    judge "安装 jq"
+  fi
+
+  # 防止部分系统xray的默认bin目录缺失
+  mkdir /usr/local/bin >/dev/null 2>&1
+}
+
+function basic_optimization() {
+  # 最大文件打开数
+  sed -i '/^\*\ *soft\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+  sed -i '/^\*\ *hard\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+  echo '* soft nofile 65536' >>/etc/security/limits.conf
+  echo '* hard nofile 65536' >>/etc/security/limits.conf
+
+  # RedHat 系发行版关闭 SELinux
+  if [[ "${ID}" == "centos" || "${ID}" == "ol" ]]; then
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+    setenforce 0
+  fi
+}
+
+
+function update_sh() {
+  ol_version=$(curl -L -s ${github_repo}/${github_branch}/install.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
+  if [[ "$shell_version" != "$(echo -e "$shell_version\n$ol_version" | sort -rV | head -1)" ]]; then
+    print_ok "存在新版本，是否更新 [Y/N]?"
+    read -r update_confirm
+    case $update_confirm in
+    [yY][eE][sS] | [yY])
+      wget -N --no-check-certificate ${github_repo}/${github_branch}/install.sh
+      print_ok "更新完成"
+      print_ok "您可以通过 bash $0 执行本程序"
+      exit 0
+      ;;
+    *) ;;
+    esac
+  else
+    print_ok "当前版本为最新版本"
+    print_ok "您可以通过 bash $0 执行本程序"
+  fi
+}
+
+function clash_install(){
+
+go install github.com/Dreamacro/clash@latest
+
+cd  $GOPATH/bin
+
+
+echo "[Unit]
+Description=Clash daemon, A rule-based proxy in Go.
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=/usr/local/bin/clash -d /etc/clash
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/clash.service
+print_ok "生成 clash.service 成功"
+
+systemctl daemon-reload
+systemctl enable clash
+systemctl start clash
+}
+
+function clash_config(){
+  
+}
+
+
+function restart_all() {
+  systemctl restart nginx
+  judge "Nginx 启动"
+  systemctl restart clash
+  judge "clash 启动"
+}
+
+
+function show_access_log() {
+  [ -f ${xray_access_log} ] && tail -f ${xray_access_log} || echo -e "${RedBG}log 文件不存在${Font}"
+}
+
+
+function install_xray() {
+  is_root
+  system_check
+  dependency_install
+  basic_optimization
+  domain_check
+  port_exist_check 80
+  xray_install
+  configure_xray
+  nginx_install
+  configure_nginx
+  configure_web
+  generate_certificate
+  ssl_judge_and_install
+  restart_all
+  basic_information
+}
+
+menu() {
+  update_sh
+  shell_mode_check
+  echo -e "\t clash 安装管理脚本 ${Red}[${shell_version}]${Font}"
+  echo -e "\t---authored by aixohub---"
+  echo -e "\thttps://github.com/aixohub\n"
+
+  echo -e "当前已安装版本：${shell_mode}"
+  echo -e "—————————————— 安装向导 ——————————————"""
+  echo -e "${Green}0.${Font}  升级 脚本"
+  echo -e "${Green}1.${Font}  安装 Xray (VLESS + TCP + XTLS / TLS + Nginx)"
+  echo -e "${Green}2.${Font}  安装 Xray (VLESS + TCP + XTLS / TLS + Nginx 及 VLESS + TCP + TLS + Nginx + WebSocket 回落并存模式)"
+  echo -e "—————————————— 配置变更 ——————————————"
+  echo -e "${Green}11.${Font} 变更 UUID"
+  echo -e "${Green}13.${Font} 变更 连接端口"
+  echo -e "${Green}14.${Font} 变更 WebSocket PATH"
+  echo -e "—————————————— 查看信息 ——————————————"
+  echo -e "${Green}21.${Font} 查看 实时访问日志"
+  echo -e "${Green}22.${Font} 查看 实时错误日志"
+  echo -e "${Green}23.${Font} 查看 Xray 配置链接"
+  #    echo -e "${Green}23.${Font}  查看 V2Ray 配置信息"
+  echo -e "—————————————— 其他选项 ——————————————"
+  echo -e "${Green}31.${Font} 安装 4 合 1 BBR、锐速安装脚本"
+  echo -e "${Yellow}32.${Font} 安装 MTproxy （不推荐使用,请相关用户关闭或卸载）"
+  echo -e "${Green}33.${Font} 卸载 Xray"
+  echo -e "${Green}34.${Font} 更新 Xray-core"
+  echo -e "${Green}35.${Font} 安装 Xray-core 测试版 (Pre)"
+  echo -e "${Green}36.${Font} 手动更新 SSL 证书"
+  echo -e "${Green}40.${Font} 退出"
+  read -rp "请输入数字：" menu_num
+  case $menu_num in
+  0)
+    update_sh
+    ;;
+  1)
+    install_xray
+    ;;
+  2)
+    install_xray_ws
+    ;;
+  11)
+    read -rp "请输入 UUID:" UUID
+    if [[ ${shell_mode} == "tcp" ]]; then
+      modify_UUID
+    elif [[ ${shell_mode} == "ws" ]]; then
+      modify_UUID
+      modify_UUID_ws
+    fi
+    restart_all
+    ;;
+  13)
+    modify_port
+    restart_all
+    ;;
+  14)
+    if [[ ${shell_mode} == "ws" ]]; then
+      read -rp "请输入路径(示例：/aixohub/ 要求两侧都包含 /):" WS_PATH
+      modify_fallback_ws
+      modify_ws
+      restart_all
+    else
+      print_error "当前模式不是 Websocket 模式"
+    fi
+    ;;
+  21)
+    tail -f $xray_access_log
+    ;;
+  22)
+    tail -f $xray_error_log
+    ;;
+  40)
+    exit 0
+    ;;
+  *)
+    print_error "请输入正确的数字"
+    ;;
+  esac
+}
+menu "$@"
