d089a3a5bfc6f639f9eae8870709e81972b5f6cf
wangpwm <wangpwm@yonyou.com>
2024-04-25 08:21:47 +0800

    update domian

--- v2ray-ws.sh
+++ v2ray-ws.sh
@@ -243,7 +243,7 @@ function basic_optimization() {
 
 function domain_check() {
   read -rp "请输入你的域名信息(eg: www.aixohub.com):" domain
-  domain_ip=$(curl -sm8 ipget.net/?ip="${domain}")
+  domain_ip=$(curl -sm8 http://ip-api.com/line/"${domain}"?fields=query)
   print_ok "正在获取 IP 地址信息，请耐心等待"
   wgcfv4_status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
   wgcfv6_status=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
