85e7520fb6552a7bf1219fef8696ac6247c82bec
wangpwm <wangpwm@yonyou.com>
2024-10-31 08:57:07 +0800

    add git

--- v2ray-ws.sh
+++ v2ray-ws.sh
@@ -478,13 +478,13 @@ server {
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   }
 
-  location /airflow/ {
+  location /airflow {
       proxy_pass http://localhost:8080;
-      proxy_set_header Host $http_host;
       proxy_redirect off;
       proxy_http_version 1.1;
-      proxy_set_header Upgrade $http_upgrade;
-      proxy_set_header Connection "upgrade";
+      proxy_set_header Host \$host;
+      proxy_set_header Upgrade \$http_upgrade;
+      proxy_set_header Connection \"upgrade\";
   }
 
 }" > ${domain}.conf
